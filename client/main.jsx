f7798329df787574c9801609d8398d1ea175d4d3
haneimo <haneimo@gmail.com>
2023-02-12 08:21:22 +0900

    ignore nescss

--- client/main.jsx
+++ client/main.jsx
@@ -3,9 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { Meteor } from 'meteor/meteor';
 import { App } from '/imports/ui/App';
 
-//import 'bootstrap';
+import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap-grid.css';
-import 'nes.css/css/nes.css'
 
 Meteor.startup(() => {
   const container = document.getElementById('react-target');
